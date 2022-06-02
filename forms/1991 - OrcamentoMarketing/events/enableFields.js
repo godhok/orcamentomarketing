255da93d106ab09a1345cb6448269a0c8641769b
Júlio César <juliocesaar.ma@gmail.com>
2022-06-02 17:03:01 -0300

    adicionar enableFields

--- forms/1991 - OrcamentoMarketing/events/enableFields.js
+++ forms/1991 - OrcamentoMarketing/events/enableFields.js
@@ -4,12 +4,37 @@ function enableFields(form) {
   var disabledFields = ["nome_solicitante", "nome_fornecedor", "cnpj_fornecedor", "contato_fornecedor", "loja_destino", "produto_servico", "service_value", "data_pagamento", "tipo_orcamento", "obs_solicitante", "nome", "tipoconta", "numero_banco", "nome_banco", "agencia", "conta", "tipopix", "chavepix", "obs"]
 
   switch (activity) {
-    case 0:
-      form.setEnabled(disabledFields[0], false)
-      form.setEnabled(disabledFields[1], false)
-      form.setEnabled(disabledFields[2], false)
-
-      break
+    case 11:
+      for (i in disabledFields) {
+        form.setEnabled(disabledFields[i], false)
+      }
+      break;
+    case 15:
+      disabledFields.push("aprovar", "obs_aprovador")
+      for (i in disabledFields) {
+        form.setEnabled(disabledFields[i], false)
+      }
+      break;
+    case 19:
+      disabledFields.push("aprovar", "obs_aprovador", "agendamento", "detalhes_agendamento", "dadoscorretos")
+      for (i in disabledFields) {
+        form.setEnabled(disabledFields[i], false)
+      }
+      break;
+    case 21:
+      form.setEnabled("aprovar", false)
+      form.setEnabled("obs_aprovador", false)
+      form.setEnabled("detalhes_agendamento", false)
+      form.setEnabled("dadoscorretos", false)
+      break;
+    case 27:
+    case 29:
+    case 32:
+      disabledFields.push("aprovar", "obs_aprovador", "agendamento", "detalhes_agendamento", "dadoscorretos", "pagamento", "detalhes_pagamento")
+      for (i in disabledFields) {
+        form.setEnabled(disabledFields[i], false)
+      }
+      break;
   }
 }
 
